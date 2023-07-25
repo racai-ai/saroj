2fe1fdb2ae32f3c83631cc7af06596535cbb375c
pvf2005 <pvf2005@gmail.com>
2023-07-25 12:05:32 +0300

    Allow the test docker to obtain automatically the host ip

--- Docker/Test_WebService_dummy/run.sh
+++ Docker/Test_WebService_dummy/run.sh
@@ -3,8 +3,10 @@
 docker stop sarojtestsdummy-run
 docker rm sarojtestsdummy-run
 
+hostip=$(ip addr show docker0 | grep -Po 'inet \K[\d.]+')
+
 docker run --name "sarojtestsdummy-run" \
-    -e "WSURL=http://127.0.0.1:8111/" \
+    -e "WSURL=http://$hostip:8111/" \
     sarojtestsdummy
 
 
