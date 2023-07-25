9ea6e6ebdebbb440db90df5e690d1bda4a95b756
pvf2005 <pvf2005@gmail.com>
2023-07-25 11:03:50 +0300

    CURL calls for the Dummy web service

---
+++ Docker/WebService_dummy/curl_tests.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+echo "Testing the Dummy WebService Docker with CURL"
+
+echo "startAnonymization"
+curl -X POST -F "input={\"caseid\":\"CASEID\",\"docid\":\"DOCID\",\"document\":\"DOCBASE64\"}" http://127.0.0.1:8111/startAnonymization.php
+echo ""
+
+echo "getResult"
+curl -X POST -F "input={\"id\":\"JOBID\"}" http://127.0.0.1:8111/getResult.php
+echo ""
+
+echo "doAnonymization"
+curl -X POST -F "input={\"caseid\":\"CASEID\",\"docid\":\"DOCID\",\"document\":\"DOCBASE64\"}" http://127.0.0.1:8111/doAnonymization.php
+echo ""
+
+echo "checkHealth"
+curl -X GET http://127.0.0.1:8111/checkHealth.php
+echo ""
+
+echo "getVersion"
+curl -X GET http://127.0.0.1:8111/getVersion.php
+echo ""
+
+
