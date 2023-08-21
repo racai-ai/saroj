4982fe7ccdcba3ac6dd5540124b54d954933f650
ValentinB <valentin.gabriel.badea@gmail.com>
2023-08-21 17:09:28 +0300

    SAROJ-1 Add extra details on help

--- WebServiceModules/TextExtractor/textExtractor_api.py
+++ WebServiceModules/TextExtractor/textExtractor_api.py
@@ -70,9 +70,10 @@ def check_health():
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('PORT', type=int, help='port to listen for requests')
-    parser.add_argument('--RUN_ANALYSIS', '-r', action='store_true', help='Y to run text analysis using UDPIPE')
+    parser.add_argument('--RUN_ANALYSIS', '-r', action='store_true',
+                        help='if present, will run text analysis using UDPIPE')
     parser.add_argument('--SAVE_INTERNAL_FILES', '-s', action='store_true',
-                        help='Y to save internal files, useful for debugging')
+                        help='if present, will save internal files, useful for debugging')
     parser.add_argument("--udpipe_model", type=str, help="Path to the UDPipe model file.")
 
     args = parser.parse_args()
