b970e0a0dd578612615453398cea35b1f0523123
ValentinB <valentin.gabriel.badea@gmail.com>
2023-10-19 21:17:31 +0300

    SAROJ-4 Rewrite entirely the solution based on the new assumtions

--- WebServiceModules/EntityMapping/entityMapping_process.py
+++ WebServiceModules/EntityMapping/entityMapping_process.py
@@ -2,15 +2,61 @@ import random
 import re
 import shutil
 import tempfile
-from collections import defaultdict
 
 from lib.saroj.suffix_process import suffix_replace, VOID_NER
 
 old_rep = None
-used = defaultdict(list)
-count_i = 0
-
+counter_inst = 0
 NOT_FOUND = "XXX"
+
+
+def get_random_X():
+    return 'X' * random.randint(3, 10)
+
+
+def count_inst_entities(filename):
+    result = []
+    current_value = 0
+
+    with open(filename, 'r', encoding="utf-8") as file:
+        lines = file.readlines()
+
+    for line in reversed(lines):
+        if line.startswith("#") or not line.strip():
+            continue
+        columns = line.strip().split('\t')
+        if columns[-1] in VOID_NER:
+            continue
+        if columns[-2].startswith("I-"):
+            current_value += 1
+            result.append(current_value)
+        elif columns[-2].startswith("B-"):
+            result.append(current_value + 1)
+            current_value = 0
+        else:
+            current_value = 0
+            result.append(current_value)
+    return result
+
+
+def count_instances_in_dict(input_dict):
+    instance_count = {}
+
+    for key, values in input_dict.items():
+        key = str(key)  # Ensure the key is a string
+        if key in instance_count:
+            instance_count[key] += len(values)
+        else:
+            instance_count[key] = len(values)
+
+    result = ""
+    for key, count in instance_count.items():
+        result += f"{key}:{count}, "
+
+    if result:
+        result = result[:-2]  # Remove the trailing comma and space
+
+    return result
 
 
 # Function to read the replacement dictionary from a file
@@ -67,6 +113,32 @@ def search_mapping_file(mapping_file, ner_id_and_potential_suffix):
     return replacement
 
 
+def filter_neutral_valid_replacements(replacements):
+    filtered_replacements = [replacement for replacement in replacements
+                             if not replacement.split()[0].endswith('a')]
+
+    valid_replacements = [replacement for replacement in filtered_replacements
+                          if len(replacement.split()) == counter_inst]
+
+    if valid_replacements:
+        return random.choice(valid_replacements)
+
+    return random.choice(replacements) if replacements else get_random_X()
+
+
+def hashtag_ner(ner_id_and_potential_suffix):
+    return ner_id_and_potential_suffix.split('_', 1)[0]
+
+
+def get_ner(ner_id_and_potential_suffix):
+    return re.sub(r'[^a-zA-Z]', '', hashtag_ner(ner_id_and_potential_suffix))
+
+
+def write_output_columns(output_f, columns):
+    output_line = '\t'.join(columns)
+    output_f.write(output_line + '\n')
+
+
 def process_already_mapped_replacement(replacement, ner_inst, ner_id_and_potential_suffix):
     """
     Process a replacement based on NER instance and potential suffix.
@@ -79,73 +151,77 @@ def process_already_mapped_replacement(replacement, ner_inst, ner_id_and_potenti
     Returns:
         str: The processed replacement.
     """
-    # Check if replacement is None or empty, return None in such cases
+
+    def is_single_token_replacement():
+        return len(replacement_tokens) == 1
+
+    def has_suffix():
+        return "_" in ner_id_and_potential_suffix and NOT_FOUND not in replacement
+
+    def apply_suffix(token, suffix):
+        return token + suffix if not token.endswith("a") else token[:-1] + suffix
+
+    def apply_first_token_suffix(suffix):
+        return apply_suffix(replacement_tokens[0], suffix)
+
+    def handle_single_token_with_suffix():
+        return apply_suffix(replacement, suffix)
+
+    def handle_multi_token_with_suffix():
+        return apply_first_token_suffix(suffix) + " " + end_token
+
     if not replacement:
         return None
 
-    # Check if ner_inst starts with "I-" and replacement has a single token
-    if ner_inst.startswith("I-") and len(replacement.split()) == 1:
-        return None
+    replacement_tokens = replacement.split()
+    end_token = " ".join(replacement_tokens[1:])
 
-    suffix = ner_id_and_potential_suffix.split("_", 1)[1] \
-        if ("_" in ner_id_and_potential_suffix) and (NOT_FOUND not in replacement) \
-        else ''
+    suffix = ner_id_and_potential_suffix.split("_")[1] if has_suffix() else None
 
-    # Check if replacement contains more than one token
-    if len(replacement.split(" ")) > 1:
-        # Construct and return the appropriate token based on conditions
-        return replacement.split(" ")[0] + suffix \
-            if not replacement.split(" ")[0].endswith("a") \
-            else replacement.split(" ")[0][:-1] + suffix
+    if ner_inst.startswith("I-") and is_single_token_replacement():
+        return "_"
 
-    # Check if ner_id_and_potential_suffix contains underscores and "XXX" is not in replacement
-    if "_" in ner_id_and_potential_suffix and NOT_FOUND not in replacement:
-        # Construct and return the appropriate token based on conditions
-        return replacement.split(" ")[0] + suffix \
-            if not replacement.endswith("a") \
-            else replacement.split(" ")[0][:-1] + suffix
+    if is_single_token_replacement() and counter_inst == 1 and suffix:
+        return handle_single_token_with_suffix()
 
-    # If none of the above conditions were met, return the second token or the full replacement
-    return replacement.split(" ")[1] if len(replacement.split(" ")) > 1 else replacement
+    if len(replacement_tokens) > 1 and counter_inst == 1 and suffix:
+        return handle_multi_token_with_suffix()
+
+    if counter_inst != 1 and NOT_FOUND not in replacement and suffix:
+        return apply_first_token_suffix(suffix)
+
+    if len(replacement_tokens) > 1 and counter_inst == 1:
+        return replacement
+
+    if is_single_token_replacement() and counter_inst != 1:
+        return replacement
+
+    if len(replacement_tokens) >= counter_inst:
+        return replacement_tokens[-counter_inst]
+
+    return "_"
 
 
-def process_entity_inst_I(ner_id_and_potential_suffix, mapping_file, replacement_dict):
+def process_entity_inst_I(ner_id_and_potential_suffix, mapping_file):
     """
     Process entities with an instance marker 'I-'.
 
     Args:
         ner_id_and_potential_suffix (str): The NER identifier and potential suffix.
         mapping_file (str): The mapping file to update.
-        replacement_dict (dict): A dictionary of replacements.
 
     Returns:
         str: The processed replacement.
     """
-    global count_i
-    count_i += 1
 
-    # Get the NER type from the identifier
-    ner = get_ner(ner_id_and_potential_suffix)
-
-    # Extract the suffix from the identifier
-    suffix = ner_id_and_potential_suffix.split("_", 1)[1] if "_" in ner_id_and_potential_suffix else ""
-
-    # Filter replacements that match the count_i
-    replacements = [replacement for replacement in used[ner] if len(replacement.split()) == count_i]
-
-    # Choose the replacement based on old_rep and count_i
-    replacement = next((lname for lname in [replacement.split(' ', 1)[count_i - 1] for replacement in replacements
-                                            if old_rep in replacement]), 'X' * random.randint(3, 10))
-
+    split_old_rep = old_rep.split()
+    split_old_rep.reverse()
     # Modify the replacement with the suffix if not found
-    rep = replacement.split(' ', 1)[0] + suffix if NOT_FOUND not in replacement else replacement
-
+    rep = split_old_rep[counter_inst - 1] if NOT_FOUND not in old_rep else old_rep
     # Update the mapping file and replacement dictionary
-    update_mapping_file(mapping_file, ner_id_and_potential_suffix, rep)
-    replacement_dict[ner] = [value for value in replacement_dict[ner]
-                             if not re.search(old_rep + " " + replacement, value)]
+    update_mapping_file(mapping_file, ner_id_and_potential_suffix, rep if len(split_old_rep) > 1 else "")
 
-    return rep
+    return rep if len(split_old_rep) > 1 else "_"
 
 
 def process_female_entity(lemma, ner_id_and_potential_suffix, replacement_dict, mapping_file):
@@ -162,31 +238,32 @@ def process_female_entity(lemma, ner_id_and_potential_suffix, replacement_dict, 
         str: The processed replacement.
     """
     global old_rep
-    global count_i
-    count_i = 0
 
     # Get the NER type from the identifier
     ner = get_ner(ner_id_and_potential_suffix)
 
     # Extract the suffix from the identifier
-    suffix = ner_id_and_potential_suffix.split("_", 1)[1] if "_" in ner_id_and_potential_suffix else ""
+    suffix = ner_id_and_potential_suffix.split("_")[1] if "_" in ner_id_and_potential_suffix else ""
 
-    # Choose the replacement based on old_rep, count_i, and matching 'a' suffix
-    replacement = next((fname for fname in [replacement.split(' ', 1)[count_i] for replacement in replacement_dict[ner]]
-                        if fname.endswith('a') and fname != lemma), 'X' * random.randint(3, 10))
+    replacement_list = [fname for fname in replacement_dict[ner] if fname.split()[0].endswith('a') and fname != lemma]
+    replacement = next((fname for fname in replacement_list if len(fname.split()) == counter_inst), None)
+
+    if replacement is None:
+        replacement = next((fname for fname in replacement_list), None)
+
+    if replacement is None:
+        replacement = random.choice(replacement_dict[ner]) if replacement_dict[ner] else get_random_X()
 
     old_rep = replacement
-    count_i += 1
-
     # Modify the replacement with the suffix if not found
-    rep = replacement.split(' ', 1)[0][:-1] + suffix if not (NOT_FOUND in replacement or not suffix) else replacement
+    rep = replacement.split()[0][:-1] + suffix + " " + " ".join(replacement.split()[1:]) \
+        if not (NOT_FOUND in replacement or not suffix) else replacement
 
-    # Update the mapping file, used list, and replacement dictionary
-    update_mapping_file(mapping_file, hashtag_ner(ner_id_and_potential_suffix), rep)
-    used[ner].extend([value for value in replacement_dict[ner] if re.search(replacement, value)])
+    # Update the mapping file and replacement dictionary
+    update_mapping_file(mapping_file, hashtag_ner(ner_id_and_potential_suffix), replacement)
     replacement_dict[ner] = [value for value in replacement_dict[ner] if not re.search(replacement, value)]
 
-    return rep
+    return rep if len(rep.split()) > counter_inst else rep.split()[0]
 
 
 def process_neutral_entity(ner_id_and_potential_suffix, replacement_dict, mapping_file):
@@ -202,29 +279,24 @@ def process_neutral_entity(ner_id_and_potential_suffix, replacement_dict, mappin
         str: The processed replacement.
     """
     global old_rep
-    global count_i
-    count_i = 1
     ner = get_ner(ner_id_and_potential_suffix)
-    suffix = ner_id_and_potential_suffix.split("_", 1)[1] if "_" in ner_id_and_potential_suffix else None
+    suffix = ner_id_and_potential_suffix.split("_")[1] if "_" in ner_id_and_potential_suffix else None
 
     replacements = replacement_dict[ner]
+    replacement = filter_neutral_valid_replacements(replacements)
 
-    # Filter replacements to exclude those ending with 'a'
-    valid_replacements = [replacement for replacement in replacements if not replacement.endswith('a')]
+    # Set the new value for columns[-1]
+    rep = replacement.split()[0] + suffix + " " + " ".join(replacement.split()[1:]) \
+        if not (NOT_FOUND in replacement or not suffix) else \
+        replacement
 
-    # If there are valid replacements, select one at random, else use a random 'X' string
-    replacement = random.choice(valid_replacements) if valid_replacements else 'X' * random.randint(3, 10)
-
-    # Set the new value for columns[1]
-    rep = replacement.split(" ")[0] + suffix if not (NOT_FOUND in replacement or not suffix) else replacement.split(" ")[0]
+    old_rep = rep
 
     # Update mapping and used values
-    update_mapping_file(mapping_file, hashtag_ner(ner_id_and_potential_suffix), rep)
-    old_rep = rep
-    used[ner].extend([value for value in replacement_dict[ner] if re.search(replacement, value)])
+    update_mapping_file(mapping_file, hashtag_ner(ner_id_and_potential_suffix), replacement)
     replacement_dict[ner] = [value for value in replacement_dict[ner] if not re.search(replacement, value)]
 
-    return rep
+    return rep if len(rep.split()) > counter_inst else rep.split()[0]
 
 
 def process_entity(token_tpl, ner_id_and_potential_suffix, mapping_file, replacement_dict):
@@ -245,13 +317,13 @@ def process_entity(token_tpl, ner_id_and_potential_suffix, mapping_file, replace
 
     if ner in replacement_dict:
         if ner_inst.startswith("I-"):
-            replacement = process_entity_inst_I(ner_id_and_potential_suffix, mapping_file, replacement_dict)
+            replacement = process_entity_inst_I(ner_id_and_potential_suffix, mapping_file)
         elif lemma.endswith('a'):
             replacement = process_female_entity(lemma, ner_id_and_potential_suffix, replacement_dict, mapping_file)
         else:
             replacement = process_neutral_entity(ner_id_and_potential_suffix, replacement_dict, mapping_file)
     else:
-        replacement = 'X' * random.randint(3, 10)
+        replacement = get_random_X()
         update_mapping_file(mapping_file, hashtag_ner(ner_id_and_potential_suffix), replacement)
 
     return replacement
@@ -285,19 +357,6 @@ def preprocess_line(line):
     return columns, (ner_id_and_potential_suffix, (ner_inst, form))
 
 
-def hashtag_ner(ner_id_and_potential_suffix):
-    return ner_id_and_potential_suffix.split('_', 1)[0]
-
-
-def get_ner(ner_id_and_potential_suffix):
-    return re.sub(r'[^a-zA-Z]', '', hashtag_ner(ner_id_and_potential_suffix))
-
-
-def write_output_columns(output_f, columns):
-    output_line = '\t'.join(columns)
-    output_f.write(output_line + '\n')
-
-
 def anonymize_entities(input_file, output_file, mapping_file, replacement_dict):
     """
     Anonymize entities in the input file and write the result to the output file.
@@ -311,13 +370,16 @@ def anonymize_entities(input_file, output_file, mapping_file, replacement_dict):
     Returns:
         None
     """
-    with open(input_file, 'r') as input_f, open(output_file, 'w') as output_f:
+    global counter_inst
+    counter_inst_list = count_inst_entities(input_file)
+    with open(input_file, 'r', encoding="utf-8") as input_f, open(output_file, 'w', encoding="utf-8") as output_f:
         for line in input_f:
             columns, token_info = preprocess_line(line)
             if token_info is None:
                 output_f.write(line)
                 continue
 
+            counter_inst = counter_inst_list.pop()
             ner_id_and_potential_suffix, token_tpl = token_info
             ner_inst = token_tpl[0]
 
@@ -326,9 +388,9 @@ def anonymize_entities(input_file, output_file, mapping_file, replacement_dict):
             rep = process_already_mapped_replacement(replacement, ner_inst, ner_id_and_potential_suffix)
 
             if rep:
-                columns[1] = rep
+                columns.append(rep)
                 write_output_columns(output_f, columns)
                 continue
 
-            columns[1] = process_entity(token_tpl, ner_id_and_potential_suffix, mapping_file, replacement_dict)
+            columns.append(process_entity(token_tpl, ner_id_and_potential_suffix, mapping_file, replacement_dict))
             write_output_columns(output_f, columns)
