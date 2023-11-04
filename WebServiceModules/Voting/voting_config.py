97bf54d67a2c1a83386855b01adab7d533487267
ValentinB <valentin.gabriel.badea@gmail.com>
2023-11-04 20:33:36 +0200

    SAROJ-6 Add default algo selection as DIFF

--- WebServiceModules/Voting/voting_config.py
+++ WebServiceModules/Voting/voting_config.py
@@ -7,7 +7,7 @@ ALGORITHMS = ["DIFF", "ADD", "INTERSECT", "MAJORITY"]
 def parse_command_line_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('PORT', type=int, help='Port to listen for requests')
-    parser.add_argument('--ALGORITHM', '-a', choices=ALGORITHMS, help='algorithm to use, mandatory')
+    parser.add_argument('--ALGORITHM', '-a', choices=ALGORITHMS, default='DIFF', help='algorithm to use, mandatory')
 
     return parser.parse_args()
 
