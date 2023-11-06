2635dcdfc6cc4022c7faf8e24a610af62c389884
ValentinB <valentin.gabriel.badea@gmail.com>
2023-11-06 21:34:01 +0200

    SAROJ-5 Make --DICTIONARY mandatory, remove PORT default value and "--"

--- WebServiceModules/Dictionary/dictionary_config.py
+++ WebServiceModules/Dictionary/dictionary_config.py
@@ -3,8 +3,8 @@ import argparse
 
 def parse_command_line_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--PORT', type=int, default=5000, help='Port to listen for requests')
-    parser.add_argument('--DICTIONARY', '-d', type=str, help='path for dictionary, mandatory')
+    parser.add_argument('PORT', type=int, help='Port to listen for requests')
+    parser.add_argument('--DICTIONARY', '-d', type=str, required=True, help='path for dictionary, mandatory')
 
     return parser.parse_args()
 
