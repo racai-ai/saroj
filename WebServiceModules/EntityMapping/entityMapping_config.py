3af4c9f04220481dd86905f25d444922e17b7dd2
ValentinB <valentin.gabriel.badea@gmail.com>
2023-12-16 18:59:58 +0200

    SAROJ-10 Add config file in param list

--- WebServiceModules/EntityMapping/entityMapping_config.py
+++ WebServiceModules/EntityMapping/entityMapping_config.py
@@ -6,7 +6,7 @@ def parse_command_line_args():
     parser.add_argument('PORT', type=int, help='Port to listen for requests')
     parser.add_argument('--DICTIONARY', '-d', type=str, help='path for dictionary, mandatory')
     parser.add_argument('--REPLACEMENT', '-r', type=str, default="X", help='replacement pattern, by default is "X" ')
-
+    parser.add_argument('--CONFIG', '-c', type=str, help='path for config file, mandatory')
     return parser.parse_args()
 
 
