22b6999ebbc17803dbfa3615c869c0d3d7e0653d
pvf2005 <pvf2005@gmail.com>
2024-01-20 13:35:33 +0200

    docker updates

--- Docker/WebService/test/curl_tests.sh
+++ Docker/WebService/test/curl_tests.sh
@@ -3,23 +3,27 @@
 echo "Testing the WebService Docker with CURL"
 
 echo "startAnonymization"
-curl -X POST -F "input={}" http://127.0.0.1:8111/startAnonymization.php
+curl -X POST --trace-ascii trace1.log -F "input={}" http://127.0.0.1:8111/startAnonymization.php
 echo ""
 
 echo "getResult"
-curl -X POST -F "input={\"id\":\"JOBID\"}" http://127.0.0.1:8111/getResult.php
+curl -X POST --trace-ascii trace2_post.log -F "input={\"id\":\"JOBID\"}" http://127.0.0.1:8111/getResult.php
+echo ""
+
+echo "getResult"
+curl -X GET --trace-ascii trace2_get.log "http://127.0.0.1:8111/getResult.php?input=\{\"id\":\"JOBID\"\}"
 echo ""
 
 echo "doAnonymization"
-curl -X POST -F "input={}" http://127.0.0.1:8111/doAnonymization.php
+curl -X POST --trace-ascii trace3.log -F "input={}" http://127.0.0.1:8111/doAnonymization.php
 echo ""
 
 echo "checkHealth"
-curl -X GET http://127.0.0.1:8111/checkHealth.php
+curl -X GET --trace-ascii trace4.log http://127.0.0.1:8111/checkHealth.php
 echo ""
 
 echo "getVersion"
-curl -X GET http://127.0.0.1:8111/getVersion.php
+curl -X GET --trace-ascii trace5.log http://127.0.0.1:8111/getVersion.php
 echo ""
 
 
